dac9509e8c4e8d0744eadb99a465874dd6d31a27
James YU <yujames2007@gmail.com>
2022-07-10 11:41:57 -0400

    new commit

---
+++ 027_stringConverter.py
@@ -0,0 +1,9 @@
+from pickle import APPEND
+from tkinter import INSERT
+
+
+list = [1,2,3,4,5]
+string = ""
+for x in list:
+    string += str(x)
+print(string)
---
+++ arrayPractice/001_arrayDisplay.py
@@ -0,0 +1,7 @@
+array = [1,2,3,4,5]
+for x in array:
+    print(x)
+subArray = array[0:3]
+print("access first three items individually")
+for y in subArray:
+    print(y)
