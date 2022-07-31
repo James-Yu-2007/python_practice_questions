703a5e607d127abdfc2e7102aae310a4e1a054dc
James YU <yujames2007@gmail.com>
2022-07-31 18:55:24 -0400

    -m

--- arrCheckChildrenSumProperty.py
+++ arrCheckChildrenSumProperty.py
@@ -2,10 +2,11 @@ from math import floor
 
 # auto creating the nodes in the array
 class nodeCreate:
-    def __init__(self, value, left, right):
+    def __init__(self, value, left = -1, right = -1, parent = -1):
         self.value = value
-        self.left = -1
-        self.right = -1
+        self.left = left
+        self.right = right
+        self.parent = parent
 
 inputStr = input("please input you tree separated by commas: ")
 inputStr = inputStr.replace(' ', '')
@@ -13,21 +14,31 @@ inputArr = inputStr.split(',')
 
 treeArr = []
 for i in range(0, len(inputArr)):
-    treeArr.append(nodeCreate(inputArr[i], {}, {}))
-    treeArr[i].value = inputArr[i]
-    treeArr[i].left = 2*i+1
-    treeArr[i].right = 2*i+2
+    treeArr.append(nodeCreate(inputArr[i]))
     # print(str(treeArr[i].left) + (" is the left child of the node ") + str(i))
     # print(str(treeArr[i].right) + (" is the right child of the node ") + str(i))
 
+for i in range(0, len(treeArr)):
+    treeArr[i].value = inputArr[i]
+    treeArr[i].left = 2*i+1
+    treeArr[i].right = 2*i+2
+    # print(str(treeArr[i].left) + (', ') + str(treeArr[i].right))
+    if treeArr[i].left >= len(treeArr):
+        treeArr[i].left = -1
+    if treeArr[i].right >= len(treeArr):
+        treeArr[i].right = -1
+    # print(str(treeArr[i].left) + (', ') + str(treeArr[i].right))
 
+# for i in range(0, len(treeArr)):
+#     print(treeArr[i].value, ", ", treeArr[i].left, ", ", treeArr[i].right)
 # checking for children sum property
 childrenSumValue = True
-for i in range(0, floor(len(treeArr)/2)):
-    if int(treeArr[treeArr[i].left].value) + int(treeArr[treeArr[i].right].value) != int(treeArr[i].value):
-        childrenSumValue = False
-
+for i in range(0, len(treeArr)):
+    # print(i)
+    if (treeArr[i].left != -1) and (treeArr[i].right != -1):
+        if int(treeArr[treeArr[i].left].value) + int(treeArr[treeArr[i].right].value) != int(treeArr[i].value):
+            childrenSumValue = False
 if childrenSumValue == True:
-    print("ur gud")
+    print("your tree satisfies the children sum property")
 else:
-    print("ur bad")
+    print("your tree does not satisfy the children sum property")
--- testing.py
+++ testing.py
@@ -1,4 +1,6 @@
-from math import floor
-
-
-print (floor(1.9))
+num = int(input("input: "))
+num2 = int(input("input2: "))
+if (num != 0) and (num2 != 0):
+    print ("ur gud")
+else:
+    print("ur bad")
