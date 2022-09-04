bcb5483bf3f8fba1dcd7f02595feeab024abdcf4
James-Yu-2007 <yujames2007@gmail.com>
2022-09-04 13:05:55 -0400

    new commit

--- searchAndSorting/05_selectionSort.py
+++ searchAndSorting/05_selectionSort.py
@@ -1,27 +1,22 @@
 list = [3,6,2,5,1,4]
 
-def minimum(list):
-    smallest = None
-    for i in list:
-        if smallest:
-            if i < smallest:
-                smallest = i
-        else:
-            smallest = i
-    return smallest
-
-def indexOfMin(list):
-    index = -1
-    for i in list:
-        index += 1
-        if i == minimum(list):
-            return index
-
 def selectionSort(list):
     for i in range(len(list)):
+        minimum = None
+        for j in list[i:]:
+            if minimum:
+                if j < minimum:
+                    minimum = j
+            else:
+                minimum = j
+        index = -1
+        for k in list:
+            index += 1
+            if k == minimum:
+                break
         n = list[i]
-        list[i] = list[indexOfMin(list[i:]) + i]
-        list[indexOfMin(list[i:]) + i] = n
+        list[i] = list[index]
+        list[index] = n
     return list
 
 print(selectionSort(list))
