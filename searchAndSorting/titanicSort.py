3b68aab027eef2204c0795fbd1d0ed3de7f472ff
James-Yu-2007 <yujames2007@gmail.com>
2022-09-07 10:48:39 -0400

    new commit

--- searchAndSorting/titanicSort.py
+++ searchAndSorting/titanicSort.py
@@ -1,17 +1,25 @@
 import csv
 
-with open('titanic.csv', 'r') as csvFile: 
+with open('C:/Users/yujam/python_practice_questions/searchAndSorting/titanic.csv', 'r') as csvFile: 
     csvReader = csv.reader(csvFile)
     age = []
+    next(csvReader)
     for row in csvReader:
-        age.append(row[5])
+        if row[5] != '':
+            age.append(row)
+
 
 list = age
 
 for i in range(len(list)):
     for j in range(len(list)):
-        if list[j] > list[i]:
-            n = list[i]
+        if float(list[j][5]) > float(list[i][5]):
+            tmp = list[i]
             list[i] = list[j]
-            list[j] = n
-print(list)
+            list[j] = tmp
+
+with open('titanic.csv', 'w', newline='') as f:
+    fieldNames = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+    thewriter = csv.DictWriter(f, fieldnames = fieldNames)
+    for record in list:
+        thewriter.writerow({'PassengerId' : record[0], 'Survived' : record[1], 'Pclass' : record[2], 'Name' : record[3], 'Sex' : record[4], 'Age' : record[5], 'SibSp' : record[6], 'Parch' : record[7], 'Ticket' : record[8], 'Fare' : record[9], 'Cabin' : record[10], 'Embarked' : record[11]})
